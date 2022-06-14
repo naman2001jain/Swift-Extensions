1d3ce87f7e5cabf08f8231f9b03bfe7204ffd8ba
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-06-14 10:32:55 +0530

    Create Extensions.swift

---
+++ Extensions.swift
@@ -0,0 +1,156 @@
+import Foundation
+import UIKit
+
+extension String {
+    var trimmed: String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed
+    }
+}
+
+
+/*String.toDate() and Date.toString()*/
+
+extension String {
+    func toDate(format: String) -> Date? {
+        let df = DateFormatter()
+        df.dateFormat = format
+        return df.date(from: self)
+    }
+}
+
+extension Date {
+    func toString(format: String) -> String {
+        let df = DateFormatter()
+        df.dateFormat = format
+        return df.string(from: self)
+    }
+}
+
+
+
+/*String width and height*/
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
+
+extension NSAttributedString {
+    func height(withConstrainedWidth width: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, context: nil)
+
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
+
+
+/*String contains only digits*/
+
+extension String {
+    var containsOnlyDigits: Bool {
+        let notDigits = NSCharacterSet.decimalDigits.inverted
+        return rangeOfCharacter(from: notDigits, options: String.CompareOptions.literal, range: nil) == nil
+    }
+}
+
+
+/*String is alphanumeric or not*/
+extension String {
+    var isAlphanumeric: Bool {
+        !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+}
+
+
+/*UIimage squared */
+
+extension UIImage {
+    var squared: UIImage? {
+        let originalWidth  = size.width
+        let originalHeight = size.height
+        var x: CGFloat = 0.0
+        var y: CGFloat = 0.0
+        var edge: CGFloat = 0.0
+        
+        if (originalWidth > originalHeight) {
+            // landscape
+            edge = originalHeight
+            x = (originalWidth - edge) / 2.0
+            y = 0.0
+            
+        } else if (originalHeight > originalWidth) {
+            // portrait
+            edge = originalWidth
+            x = 0.0
+            y = (originalHeight - originalWidth) / 2.0
+        } else {
+            // square
+            edge = originalWidth
+        }
+        
+        let cropSquare = CGRect(x: x, y: y, width: edge, height: edge)
+        guard let imageRef = cgImage?.cropping(to: cropSquare) else { return nil }
+        
+        return UIImage(cgImage: imageRef, scale: scale, orientation: imageOrientation)
+    }
+}
+
+
+/*UIImage resized*/
+extension UIImage {
+    func resized(maxSize: CGFloat) -> UIImage? {
+        let scale: CGFloat
+        if size.width > size.height {
+            scale = maxSize / size.width
+        }
+        else {
+            scale = maxSize / size.height
+        }
+        
+        let newWidth = size.width * scale
+        let newHeight = size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
+
+
+
+/*Bundle app version*/
+extension Bundle {
+    var appVersion: String? {
+        self.infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    
+    static var mainAppVersion: String? {
+        Bundle.main.appVersion
+    }
+}
+
+
